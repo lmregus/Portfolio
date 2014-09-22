574643bd4cad746a7570661263f66966eea5dc81
Luis Regus <lmregus27@gmail.com>
2014-09-22 14:10:49 -0400

    Added connectfour

---
+++ C-C++/C++/connectFour/ConnectFourType/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "ConnectFourType.h"
+
+using namespace std;
+ 
+int main () 
+{
+        connectFour startGame;
+
+           
+
+           
+            startGame.displayMenu();
+          
+
+           
+
+        return 0;
+}
+
