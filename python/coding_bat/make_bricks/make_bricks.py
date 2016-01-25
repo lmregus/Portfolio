359889da57de30dea741f103b802ac4da1869f16
Luis Regus <lmregus27@gmail.com>
2016-01-25 17:31:39 -0500

    adds make bricks and no teen sum

---
+++ python/coding_bat/make_bricks/make_bricks.py
@@ -0,0 +1,9 @@
+#########################
+#                       #
+# Developer: Luis Regus #
+#                       #
+#########################
+
+
+def make_bricks(small, big, goal):
+        return goal <= (small +big *5) and goal % 5 <= small
